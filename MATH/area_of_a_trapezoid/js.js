6f3a78de3b15f6271385b455ae90ebd478f2c924
Mohamad Al Salty <Mohamadhazems@gmail.com>
2020-12-31 22:38:47 -0300

    update

---
+++ MATH/area_of_a_trapezoid/js.js
@@ -0,0 +1,5 @@
+let a = 5; //define the first base value
+let b = 6; //define the second base value
+let h = 5; //define the height value
+let A = (a+b)/2 * h; //#the calculate formula
+alert(A); //to print the area
