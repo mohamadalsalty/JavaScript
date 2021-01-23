1a9335f889ecd592c1eec73f7a6b9c91118c76ba
Mohamad Al Salty <Mohamadhazems@gmail.com>
2021-01-23 00:11:05 -0300

    upload

---
+++ MATH/arc_length_of_an_angle/js.js
@@ -0,0 +1,8 @@
+//Create the function
+function LengthOfAnAngle(D,A) {
+	// The formula
+	pi = 22/7//define the PI value
+	volume = (pi*D) * (A/360) //the formul
+	alert(volume);
+}
+LengthOfAnAngle(8,45)
